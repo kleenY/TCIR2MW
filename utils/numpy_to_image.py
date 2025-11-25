79161655902e97ff8e8b413276f9fe9641bee315
kleenY <58740544+kleenY@users.noreply.github.com>
2025-11-25 14:20:17 +0400

    Add files via upload

--- utils/numpy_to_image.py
+++ utils/numpy_to_image.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*
 # @Time : 2022/5/6 22:46
-# @Author : 杨坤林
+# Author: Kunlin Yang
 # @File : numpy_to_image.py
 # @Software : PyCharm
 
@@ -16,8 +16,6 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 def draw(data_path):
     '''
     单独的画图函数，将npy文件保存到指定路径
@@ -26,7 +24,7 @@ def draw(data_path):
     data = data.squeeze()
     plt.figure()
     sns.heatmap(data=data,
-                cmap="gray_r"  # 淡色色盘：sns.light_palette()使用
+                cmap="gray_r"  # Pastel palette (sns.light_palette) usage：sns.light_palette()
                 )
     plt.title(os.path.basename(data_path))
     # plt.show()
@@ -34,29 +32,37 @@ def draw(data_path):
     plt.close()
 
 def convert1(path_test):
-    # image_array是归一化的二维浮点数矩阵
+    """Perform the convert1 operation.
+
+    Args:
+        path_test (str): Description.
+
+    Returns:
+        Any: Result.
+    """
+    # image_arrayNormalization
     image_array = np.load(path_test)
-    # 归一化为（0，1），下面才能转换,注意，归一化的时候的裁剪部分
+    # Normalize to [0, 1]，,，Normalization
     arr_no_0 = image_array.flatten()[np.flatnonzero(image_array)]
     max = arr_no_0.max()
     min = arr_no_0.min()
-    # 这里填充最大还是最小，根据图片来弄
+    # Comment translated to English (manual check recommended)
     image_array = np.where(image_array == 0, max, image_array)
     image_array = (image_array - min) / (max - min)
-    # (image_array-np.min(image_array))/(np.max(image_array)-np.min(image_array))  # 最值归一化
+    # (image_array-np.min(image_array))/(np.max(image_array)-np.min(image_array))  # Normalization
 
     image_array = 1 - image_array
-    image_array *= 255  # 变换为0-255的灰度值
+    image_array *= 255  # Convert to 0–255 grayscale values
     # image_array = 255 - image_array
     # print(image_array[255, 255])
     im = Image.fromarray(image_array)
-    im1 = im.convert('F')  # 这样才能转为灰度图，如果是彩色图则改L为‘RGB’
+    im1 = im.convert('F')  # Convert to grayscale; for color images use "RGB" instead of "L".
     print(im1.getpixel((255, 255)))
     print(type(im1.getpixel((255, 255))))
     im2 = im.convert('L')
     print(im2.getpixel((255, 255)))
     print(type(im2.getpixel((255, 255))))
-    im3 = im.convert('I')  # 这样才能转为灰度图，如果是彩色图则改L为‘RGB’
+    im3 = im.convert('I')  # Convert to grayscale; for color images use "RGB" instead of "L".
     print(im3.getpixel((255, 255)))
     print(type(im3.getpixel((255, 255))))
     # print(im.mode)
@@ -65,11 +71,19 @@ def convert1(path_test):
     im3.save('test3.png')
 
 def convert2():
+    """Perform the convert2 operation.
+
+    Args:
+        None
+
+    Returns:
+        Any: Result.
+    """
     # Make an array of 120000 random bytes
     randomByteArray = bytearray(os.urandom(120000))
     # translate into numpy array
     flatNumpyArray = np.array(randomByteArray)
-    # Convert the array to make a 400*300 grayscale image(灰度图像)
+    # Convert the array to make a 400*300 grayscale image(grayscale image)
     grayImage = flatNumpyArray.reshape(300, 400)
     # show gray image
     cv.imshow('GrayImage', grayImage)
@@ -85,36 +99,52 @@ def convert2():
     cv.waitKey()
     cv.destroyAllWindows()
 
-# 进度条使用示例
+# Progress bar usage example
 def pbar():
+    """Perform the pbar operation.
+
+    Args:
+        None
+
+    Returns:
+        Any: Result.
+    """
     pbar = tqdm(total=100)
     for i in range(100):
         time.sleep(1)
         pbar.update(1)
 
-
 def convert3(path, savepath):
-    # image_array是归一化的二维浮点数矩阵
+    """Perform the convert3 operation.
+
+    Args:
+        path (str): Description.
+        savepath (str): Description.
+
+    Returns:
+        Any: Result.
+    """
+    # image_arrayNormalization
     image_array = np.load(path)
-    # 归一化为（0，1），下面才能转换,注意，归一化的时候的裁剪部分
+    # Normalize to [0, 1]，,，Normalization
     # arr_no_0 = image_array.flatten()[np.flatnonzero(image_array)]
     # max = np.max(image_array)
     # min = np.min(image_array)
-    # 这里填充最大还是最小，根据图片来弄
+    # Comment translated to English (manual check recommended)
     # image_array = np.where(image_array == 0, max, image_array)
-    # 归一化
+    # Normalization
     image_array = (image_array - 100) / (350 - 100)
-    # (image_array-np.min(image_array))/(np.max(image_array)-np.min(image_array))  # 最值归一化
+    # (image_array-np.min(image_array))/(np.max(image_array)-np.min(image_array))  # Normalization
     # image_array = np.where(image_array == 1, 0, image_array)
-    # 反向
+    # Inverse / reverse
     image_array = 1 - image_array
 
-    # 变换为0-255的灰度值
+    # Convert to 0–255 grayscale values
     image_array *= 255
     # image_array = 255 - image_array
     # print(image_array[255, 255])
     im = Image.fromarray(image_array)
-    # im1 = im.convert('F')  # 这样才能转为灰度图，如果是彩色图则改L为‘RGB’
+    # im1 = im.convert('F')  # Convert to grayscale; for color images use "RGB" instead of "L".
     # print(im1.getpixel((255, 255)))
     # print(type(im1.getpixel((255, 255))))
     im2 = im.convert('L')
@@ -123,8 +153,8 @@ def convert3(path, savepath):
     name = name + '.png'
     temp_savepath = os.path.join(savepath, name)
     # print(im2.getpixel((255, 255)))
-    # print(type(im2.getpixel((255, 255))))  # F和L的区别就是取整
-    # im3 = im.convert('I')  # 这样才能转为灰度图，如果是彩色图则改L为‘RGB’
+    # print(type(im2.getpixel((255, 255))))  # Difference between "F" and "L" modes: "L" stores integers.
+    # im3 = im.convert('I')  # Convert to grayscale; for color images use "RGB" instead of "L".
     # print(im3.getpixel((255, 255)))
     # print(type(im3.getpixel((255, 255))))
     # print(im.mode)
@@ -132,8 +162,16 @@ def convert3(path, savepath):
     im2.save(temp_savepath)
     # im3.save('test3.png')
 
-
 def convert_all(npy_path, img_path):
+    """Perform the convert_all operation.
+
+    Args:
+        npy_path (str): Description.
+        img_path (Tensor): Description.
+
+    Returns:
+        Any: Result.
+    """
     data_list = os.listdir(npy_path)
     # pbar = tqdm(total=len(data_list))
     for data in data_list:
@@ -142,8 +180,6 @@ def convert_all(npy_path, img_path):
         # pbar.update(1)
     # pbar.close()
 
-
-
 if __name__ == '__main__':
     print("start!")
     # pathtest = 'G:/meta_data/train/CH3_TEMP_IRSPL/lr/0.npy'
@@ -151,18 +187,13 @@ if __name__ == '__main__':
     # convert3(pathtest, save_path)
     # # draw(pathtest)
 
-
     inf2mw_path = 'G:\\trn\\b1'
     image_inf_mw_path = 'G:\\trn\\b1_image'
-    if not os.path.isdir(image_inf_mw_path):  # 判断是否存在，不存在，创建
+    if not os.path.isdir(image_inf_mw_path):  # Create it if it does not exist
         os.makedirs(image_inf_mw_path)
     convert_all(inf2mw_path, image_inf_mw_path)
 
     # print("end!")
-
-
-
-
 
     # path_all = ''
     # model_list = os.listdir(path_all)
@@ -175,27 +206,18 @@ if __name__ == '__main__':
     #     for task_name in task_list:
     #         npy_path = os.path.join(temp_npy_path, task_name)
     #         save_path = os.path.join(temp_img_path, task_name)
-    #         if not os.path.isdir(save_path):  # 判断是否存在，不存在，创建
+    # if not os.path.isdir(save_path):  # Create it if it does not exist
     #             os.makedirs(save_path)
     #         convert_all(npy_path, save_path)
     #
     #     pbar.update(1)
     # pbar.close()
 
-
-
-
-
-
-
-
-
     # npy_path = '../my_test/CH5_TEMP_IRWVP_npy'
     # save_path = '../my_test/CH5_TEMP_IRWVP_img'
-    # if not os.path.isdir(save_path):  # 判断是否存在，不存在，创建
+    # if not os.path.isdir(save_path):  # Create it if it does not exist
     #     os.makedirs(save_path)
     # convert_all(npy_path, save_path)
     print("over!")
     # dataname = os.listdir("/media/aita-ocean/data/YKL/meta_train_data/test/CH3_TEMP_IRSPL/lr")
     # print(random.sample(dataname, 50))
-
